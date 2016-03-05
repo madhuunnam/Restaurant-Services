5aa6229dabfa7c80b597bde1bf15ca4d4c3d661c
madhaviunnam <madhaviunnam@192.168.1.5>
2016-03-05 08:52:50 -0500

    Implemented customer signup functionality

--- src/main/java/com/restaurant/services/dao/CustomerRepository.java
+++ src/main/java/com/restaurant/services/dao/CustomerRepository.java
@@ -17,13 +17,19 @@ public class CustomerRepository {
 	
 	
 	private static final String INSERT_CUSTOMER_RECORDS = 
-			"INSERT INTO Customers(firstName, lastName,insertDate) VALUES (:firstName,:lastName,:insertDate)";
+			"INSERT INTO Customers(firstName, middleName, lastName, emailAddress, password, promoCode, adChannel, insertDate) "
+			+ "VALUES (:firstName,:middleName,:lastName,:email,:password,:promoCode,:adChannel,:insertDate)";
 	
 	public void registerCustomer(Customer customer){
 		
 		Map<String,Object> paramMap = new HashMap<String,Object>();
 		paramMap.put("firstName", customer.getFirstName());
+		paramMap.put("middleName", customer.getMiddleName());
 		paramMap.put("lastName", customer.getLastName());
+		paramMap.put("email", customer.getEmail());
+		paramMap.put("password", customer.getPassword());
+		paramMap.put("promoCode", customer.getPromoCode());
+		paramMap.put("adChannel", customer.getAdChannel());
 		paramMap.put("insertDate", customer.getInsertDate());
 		
 		namedParameterTemplate.update(INSERT_CUSTOMER_RECORDS, paramMap);
--- src/main/java/com/restaurant/services/model/Customer.java
+++ src/main/java/com/restaurant/services/model/Customer.java
@@ -5,7 +5,12 @@ import java.util.Date;
 public class Customer {
 	
 	private String firstName;
+	private String middleName;
 	private String lastName;
+	private String email;
+	private String password;
+	private String promoCode;
+	private String adChannel;
 	private Date insertDate;
 	
 	public String getFirstName() {
@@ -14,11 +19,41 @@ public class Customer {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
 	public String getLastName() {
 		return lastName;
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPromoCode() {
+		return promoCode;
+	}
+	public void setPromoCode(String promoCode) {
+		this.promoCode = promoCode;
+	}
+	public String getAdChannel() {
+		return adChannel;
+	}
+	public void setAdChannel(String adChannel) {
+		this.adChannel = adChannel;
 	}
 	public Date getInsertDate() {
 		return insertDate;
@@ -27,6 +62,11 @@ public class Customer {
 		this.insertDate = insertDate;
 	}
 	
+	public String toString() {
+		return "Customer [firstName=" + firstName + ", middleName=" + middleName + ", lastName=" + lastName + ", email="
+				+ email + ", password=" + password + ", promoCode=" + promoCode + ", adChannel=" + adChannel
+				+ ", insertDate=" + insertDate + "]";
+	}
 	
-
+	
 }
