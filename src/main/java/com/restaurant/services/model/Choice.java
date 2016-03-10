ff87f3841f2e4722d3de8d9b7251832835e9e008
madhaviunnam <madhaviunnam@192.168.1.5>
2016-03-10 08:20:00 -0500

    Created Choice Model

---
+++ src/main/java/com/restaurant/services/model/Choice.java
@@ -0,0 +1,61 @@
+package com.restaurant.services.model;
+
+public class Choice {
+
+	private String restId;
+	private String itemNum;
+	private String chNum;
+	private String chTitle;
+	private String chType;
+	private boolean required;
+	private int numValue;
+	public String getRestId() {
+		return restId;
+	}
+	public void setRestId(String restId) {
+		this.restId = restId;
+	}
+	public String getItemNum() {
+		return itemNum;
+	}
+	public void setItemNum(String itemNum) {
+		this.itemNum = itemNum;
+	}
+	public String getChNum() {
+		return chNum;
+	}
+	public void setChNum(String chNum) {
+		this.chNum = chNum;
+	}
+	public String getChTitle() {
+		return chTitle;
+	}
+	public void setChTitle(String chTitle) {
+		this.chTitle = chTitle;
+	}
+	public String getChType() {
+		return chType;
+	}
+	public void setChType(String chType) {
+		this.chType = chType;
+	}
+	public boolean isRequired() {
+		return required;
+	}
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+	public int getNumValue() {
+		return numValue;
+	}
+	public void setNumValue(int numValue) {
+		this.numValue = numValue;
+	}
+	@Override
+	public String toString() {
+		return "Choice [restId=" + restId + ", itemNum=" + itemNum + ", chNum=" + chNum + ", chTitle=" + chTitle
+				+ ", chType=" + chType + ", required=" + required + ", numValue=" + numValue + "]";
+	}
+
+	
+}
