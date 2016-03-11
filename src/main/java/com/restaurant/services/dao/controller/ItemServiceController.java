256ec0a6bc7d88c359d4581a1f762b03e4bb25eb
madhaviunnam <madhaviunnam@192.168.1.5>
2016-03-11 17:47:55 -0500

    removed unnecessary code

--- src/main/java/com/restaurant/services/dao/controller/ItemServiceController.java
+++ src/main/java/com/restaurant/services/dao/controller/ItemServiceController.java
@@ -1,6 +1,5 @@
 package com.restaurant.services.dao.controller;
 
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,27 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restaurant.services.dao.ItemRepository;
-import com.restaurant.services.model.Choice;
-import com.restaurant.services.model.ChoiceValues;
 import com.restaurant.services.model.Item;
-import com.restaurant.services.model.Restaurant;
 
 @RestController
 public class ItemServiceController {
-	
+
 	@Autowired
 	ItemRepository itemRepository;
-	
-	@RequestMapping(value = "/pingItem", method = RequestMethod.GET, produces = "application/json")
-	public Item ping() {
-
-		Item admin = new Item();
-		admin.setItemName("Dosa");
-		admin.setNumChoice(1);
-
-		return admin;
-	}
-
 
 	@RequestMapping(value = "/addItem", method = RequestMethod.POST)
 	public ResponseEntity<String> addItem(@RequestBody Item item) {
@@ -47,20 +32,20 @@ public class ItemServiceController {
 		}
 		return new ResponseEntity<String>(insertStatus, HttpStatus.OK);
 	}
-	
+
 	@RequestMapping(value = "/updateItem", method = RequestMethod.PUT)
 	public ResponseEntity<String> udpateItem(@RequestBody Item item) {
 
-		String insertStatus = "Item SetUp Success!";
+		String insertStatus = "Item update Success!";
 		try {
 			itemRepository.updateItem(item);
 		} catch (Exception e) {
 			e.printStackTrace();
-			insertStatus = "Item SetUp Failed";
+			insertStatus = "Item update Failed";
 		}
 		return new ResponseEntity<String>(insertStatus, HttpStatus.OK);
 	}
-	
+
 	@RequestMapping(value = "/getItemListForRestaurant/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
 	public List<Item> getItemListForRestaurant(@PathVariable("restaurantId") String restaurantId) {
 		String sqlStatus = "Sql Success!";
@@ -73,18 +58,18 @@ public class ItemServiceController {
 		}
 		return items;
 	}
-	
+
 	@RequestMapping(value = "/deleteItemFromRestaurant/{restaurantId}/{itemNum}", method = RequestMethod.GET)
-	public ResponseEntity<String> deleteItemFromRestaurant(@PathVariable("restaurantId") String restaurantId, @PathVariable("itemNum") String itemNo) {
+	public ResponseEntity<String> deleteItemFromRestaurant(@PathVariable("restaurantId") String restaurantId,
+			@PathVariable("itemNum") String itemNo) {
 		String sqlStatus = "Sql Success!";
 		try {
-			itemRepository.deleteItemFromRestaurant(restaurantId,itemNo);
+			itemRepository.deleteItemFromRestaurant(restaurantId, itemNo);
 		} catch (Exception e) {
 			e.printStackTrace();
 			sqlStatus = "Sql failed ";
 		}
 		return new ResponseEntity<String>(sqlStatus, HttpStatus.OK);
 	}
-	
-	
+
 }
