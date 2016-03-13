28959cb18c2d8d606fcf8fcd2cf45e2a960edbad
M_UNNAM <M_UNNAM@ADA.isp.uncg.edu>
2016-03-13 10:36:00 -0400

    Removed Unnecessasry Sysout

--- src/main/java/com/restaurant/services/Application.java
+++ src/main/java/com/restaurant/services/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-    	System.out.println("Subhash is testing Restaurant service s");
         SpringApplication.run(Application.class, args);
     }
 }
