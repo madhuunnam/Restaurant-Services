afeab53449f6ade3642311d98033691e6c427bf9
Madhavi Unnam <madhuunnam@gmail.com>
2016-03-20 17:34:03 -0400

    added CORS allowance for PUT and DELETE

--- src/main/java/com/restaurant/services/Application.java
+++ src/main/java/com/restaurant/services/Application.java
@@ -20,7 +20,9 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080","*");
+                registry.addMapping("/**")
+                	.allowedOrigins("*","http://localhost:8080")
+                	.allowedMethods("PUT", "DELETE","GET","POST");
             }
         };
     }
