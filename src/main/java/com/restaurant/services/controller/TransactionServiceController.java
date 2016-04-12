3808360b48552acae1a90d549ad150a3a5203516
madhaviunnam <madhaviunnam@192.168.1.5>
2016-04-12 12:08:09 -0400

    Code changes for RestStatistics implementation

--- src/main/java/com/restaurant/services/controller/TransactionServiceController.java
+++ src/main/java/com/restaurant/services/controller/TransactionServiceController.java
@@ -3,6 +3,7 @@ package com.restaurant.services.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +31,116 @@ public class TransactionServiceController {
 		return new ResponseEntity<String>(insertStatus, HttpStatus.OK);
 	}
 	
+	@RequestMapping(value = "/getPickUpOrdersTotalPrice/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getPickUpOrdersTotalPrice(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String pickUpSum="";
+		try {
+			pickUpSum = transactionRepository.getPickUpOrdersTotalPrice(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return pickUpSum;
+	}
+	
+	@RequestMapping(value = "/getNumberOfPickUpOrders/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getNumberOfPickUpOrders(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String noOfpickUpOrders="";
+		try {
+			noOfpickUpOrders = transactionRepository.getNumberOfPickUpOrders(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return noOfpickUpOrders;
+	}
+	
+	@RequestMapping(value = "/getDeliveryOrdersTotalPrice/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getDeliveryOrdersTotalPrice(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String deliverySum="";
+		try {
+			deliverySum = transactionRepository.getDeliveryOrdersTotalPrice(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return deliverySum;
+	}
+	
+	@RequestMapping(value = "/getNumberOfDeliveryOrders/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getNumberOfDeliveryOrders(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String noOfDeliveryOrders="";
+		try {
+			noOfDeliveryOrders = transactionRepository.getNumberOfDeliveryOrders(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return noOfDeliveryOrders;
+	}
+	
+	@RequestMapping(value = "/getReservationOrdersTotalPrice/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getReservationOrdersTotalPrice(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String reservationSum="";
+		try {
+			reservationSum = transactionRepository.getReservationOrdersTotalPrice(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return reservationSum;
+	}
+	
+	@RequestMapping(value = "/getNumberOfReservationOrders/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getNumberOfReservationOrders(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String noOfReservationOrders="";
+		try {
+			noOfReservationOrders = transactionRepository.getNumberOfReservationOrders(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return noOfReservationOrders;
+	}
+	@RequestMapping(value = "/getOrdersTotalPrice/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getOrdersTotalPrice(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String totalSum="";
+		try {
+			totalSum = transactionRepository.getOrdersTotalPrice(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return totalSum;
+	}
+	
+	@RequestMapping(value = "/getNumberOfOrders/{restaurantId}", method = RequestMethod.GET, produces = "application/json")
+	public String getNumberOfOrders(@PathVariable("restaurantId") String restId) {
+		
+		String sqlStatus = "Sql Success!";
+		String noOfOrders="";
+		try {
+			noOfOrders = transactionRepository.getNumberOfOrders(restId);
+		} catch (Exception e) {
+			e.printStackTrace();
+			sqlStatus = "Sql failed ";
+		}
+		return noOfOrders;
+	}
 	
 
 }
--- src/main/java/com/restaurant/services/dao/repository/TransactionRepository.java
+++ src/main/java/com/restaurant/services/dao/repository/TransactionRepository.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +22,17 @@ public class TransactionRepository {
 			+ ":orderType,:orderTime,:numberOfLines,:itemName,:subTot,:discountPercentage,:taxRatePercent,:taxAmount,:tip,:deliFee,:totPrice,:receiverName,:deliAddr,"
 			+ ":status,:msgToCust,:msgToRes,:nonSmoke,:resTable,:resPeople,:resTime,:pickTime,:arriveTime,:agentName,:notes)";
 
+	private static final String PICKUP_TRANSACTIONS_SUM = "SELECT ROUND(sum(totPrice),2) from Transactions where orderType = 'PickUp' and resID=:restId";
+	private static final String PICKUP_TRANSACTIONS_COUNT = "SELECT count(*) from Transactions where orderType = 'PickUp' and resID=:restId";
+	private static final String DELIVERY_TRANSACTIONS_SUM = "SELECT ROUND(sum(totPrice),2) from Transactions where orderType = 'Delivery' and resID=:restId";
+	private static final String DELIVERY_TRANSACTIONS_COUNT = "SELECT count(*) from Transactions where orderType = 'Delivery' and resID=:restId";
+	private static final String RESERVATION_TRANSACTIONS_SUM = "SELECT ROUND(sum(totPrice),2) from Transactions where orderType = 'Reservation' and resID=:restId";
+	private static final String RESERVATION_TRANSACTIONS_COUNT = "SELECT count(*) from Transactions where orderType = 'Reservation' and resID=:restId";
+	private static final String TOTAL_TRANSACTIONS_SUM = "SELECT ROUND(sum(totPrice),2) from Transactions where resID=:restId";
+	private static final String TOTAL_TRANSACTIONS_COUNT = "SELECT count(*) from Transactions where resID=:restId";
+	
+	
+
 	public void addTransaction(Transaction order) {
 
 		Map<String, Object> paramMap = createParameterMap(order);
@@ -28,6 +40,120 @@ public class TransactionRepository {
 		namedParameterTemplate.update(INSERT_TRANSACTION_RECORDS, paramMap);
 
 	}
+	
+	public String getPickUpOrdersTotalPrice(String restId) {
+
+		String pickUpSum = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			 
+			pickUpSum = namedParameterTemplate.queryForObject(PICKUP_TRANSACTIONS_SUM, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return pickUpSum;
+	}
+	public String getNumberOfPickUpOrders(String restId) {
+
+		String noOfpickUpOrders = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			noOfpickUpOrders = namedParameterTemplate.queryForObject(PICKUP_TRANSACTIONS_COUNT, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return noOfpickUpOrders;
+	}
+	
+	public String getDeliveryOrdersTotalPrice(String restId) {
+
+		String deliverySum = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			 
+			deliverySum = namedParameterTemplate.queryForObject(DELIVERY_TRANSACTIONS_SUM, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return deliverySum;
+	}
+	public String getNumberOfDeliveryOrders(String restId) {
+
+		String noOfdeliveryOrders = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			noOfdeliveryOrders = namedParameterTemplate.queryForObject(DELIVERY_TRANSACTIONS_COUNT, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return noOfdeliveryOrders;
+	}
+	
+	public String getReservationOrdersTotalPrice(String restId) {
+
+		String reservationSum = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			 
+			reservationSum = namedParameterTemplate.queryForObject(RESERVATION_TRANSACTIONS_SUM, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return reservationSum;
+	}
+	public String getNumberOfReservationOrders(String restId) {
+
+		String noOfReservationOrders = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			noOfReservationOrders = namedParameterTemplate.queryForObject(RESERVATION_TRANSACTIONS_COUNT, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return noOfReservationOrders;
+	}
+	
+	public String getOrdersTotalPrice(String restId) {
+
+		String totalSum = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			 
+			totalSum = namedParameterTemplate.queryForObject(TOTAL_TRANSACTIONS_SUM, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return totalSum;
+	}
+	public String getNumberOfOrders(String restId) {
+
+		String noOfOrders = "";
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("restId", restId);
+		try {
+			noOfOrders = namedParameterTemplate.queryForObject(TOTAL_TRANSACTIONS_COUNT, paramMap, String.class);
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return noOfOrders;
+	}
+	
+	
 
 	private Map<String, Object> createParameterMap(Transaction order) {
 
